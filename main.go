590c37f9cf9f9c54a6fec87943a378bc17e0166c
Maksim Pinigin <pinigin@nvie.ru>
2018-07-28 00:35:22 +0700

    LoadConfig() in main.go

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 )
+
+var config Config
 
 func main() {
 	fmt.Println("pinit 0.1 Copyright (c) 2018 Maksim Pinigin <pinigin@nvie.ru>")
+
+	var loadConfResult bool
+	switch runtime.GOOS {
+	case "linux":
+		loadConfResult = LoadConfig("/etc/pinit/configuration.json")
+	case "windows":
+		loadConfResult = LoadConfig("C:\\pinit\\configuration.json")
+	default:
+		fmt.Println("pinit: not supported operating system, sorry bro")
+		os.Exit(3)
+	}
+
+	if loadConfResult != true {
+		fmt.Println("pinit: Fatal error: unknown error loadConfResult = false")
+		os.Exit(2)
+	}
 }

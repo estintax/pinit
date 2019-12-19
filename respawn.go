3d7e6c3f507b15865008e3fddd1d2c3b68a37384
Maksim Pinigin <pinigin@mapicom.ru>
2019-12-19 20:55:14 +0700

    Optimized respawn process

--- respawn.go
+++ respawn.go
@@ -8,12 +8,16 @@ func StartRespawnProcess(exec string, args []string) {
 	var procAttr os.ProcAttr
 	procAttr.Dir = "/"
 	procAttr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr}
-	process, err := os.StartProcess(exec, args, &procAttr)
-	if err != nil {
-		Warning("Failed to start respawn process " + exec, err)
-	}
-	state, _ := process.Wait()
-	if state.Exited() == true {
-		StartRespawnProcess(exec, args)
+
+	for {
+		process, err := os.StartProcess(exec, args, &procAttr)
+		if err != nil {
+			Warning("Failed to start respawn process " + COLOR_WHITE + exec + COLOR_RESET, err)
+			return
+		}
+		state, _ := process.Wait()
+		if state.Exited() == true {
+			continue
+		}
 	}
 }

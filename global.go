7197aa81769a2af5b09205bfd740d3ae7791e48a
Maksim Pinigin <pinigin@mapicom.ru>
2019-12-23 22:09:59 +0700

    Created a test mode on the knee

--- global.go
+++ global.go
@@ -3,3 +3,5 @@ package main
 var config map[string]interface{}
 var servicesPath string
 var servicesPids map[string]int
+var shutdownCmds []Command
+var testMode bool = false
--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 func main() {
 	fmt.Printf("%sp%si%sn%si%st%s (c) 2018-2019 Maksim Pinigin\n", COLOR_LIGHT_GREEN, COLOR_LIGHT_RED, COLOR_LIGHT_CYAN, COLOR_LIGHT_PURPLE, COLOR_YELLOW, COLOR_RESET)
 
+	if os.Getpid() != 1 {
+		fmt.Printf("Running in %stest%s mode\n", COLOR_LIGHT_RED, COLOR_RESET)
+		testMode = true
+	}
+
 	servicesPids = make(map[string]int)
 
 	var loadConfResult bool

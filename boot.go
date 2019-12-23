791cd92cf107e780f6ccb4ae9fce203076c61f80
Maksim Pinigin <pinigin@mapicom.ru>
2019-12-23 22:09:13 +0700

    Using syscall package to change hostname

--- boot.go
+++ boot.go
@@ -4,7 +4,7 @@ import (
   "fmt"
   "os"
   "strings"
-  "./linux"
+  "syscall"
 )
 
 func SetHostname() {
@@ -19,16 +19,17 @@ func SetHostname() {
   n, err := file.Read(data)
   if err != nil {
     Warning("Failed to read /etc/hostname", err)
+    file.Close()
+    return
   }
 
   hostname := string(data[:n])
   hostname = strings.Trim(hostname, "\n")
   file.Close()
   fmt.Printf("         Setting hostname to %s%s%s...\n", COLOR_WHITE, hostname, COLOR_RESET)
-  result := linux.SetHostname(hostname)
-  if result == -1 {
+  err = syscall.Sethostname([]byte(hostname))
+  if err != nil {
     fmt.Printf("[%sFAILED%s] Failed to set hostname. Maybe, you are not a root\n", COLOR_LIGHT_RED, COLOR_RESET)
-    //Warning("Failed to set hostname. Maybe, you not a root", nil)
   } else {
     fmt.Printf("[  %sOK%s  ] Hostname is set to %s%s%s\n", COLOR_LIGHT_GREEN, COLOR_RESET, COLOR_WHITE, hostname, COLOR_RESET)
   }

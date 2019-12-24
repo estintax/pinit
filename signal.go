cfb172cb2347519e2438c8ee490df1b833ddcac7
Maksim Pinigin <pinigin@mapicom.ru>
2019-12-24 13:21:55 +0700

    Improved InterruptHandle function

--- signal.go
+++ signal.go
@@ -8,9 +8,16 @@ import (
 
 func InterruptHandle() {
   sig := make(chan os.Signal, 2)
-  signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+  signal.Notify(sig, os.Interrupt, syscall.SIGTERM, syscall.SIGUSR1, syscall.SIGUSR2)
   go func ()  {
-    <-sig
-    Exit(1)
+    s := <-sig
+    switch s {
+    case os.Interrupt, syscall.SIGTERM:
+      Exit(2)
+    case syscall.SIGUSR1:
+      Exit(0)
+    case syscall.SIGUSR2:
+      Exit(1)
+    }
   }()
 }
